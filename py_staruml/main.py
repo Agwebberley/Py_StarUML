a630f9a8dad1883607e311207e1dd7cc892969be
Andrew Webberley <agwebberley@pm.me>
2024-03-21 13:38:24 -0700

    Start ProjectGenerator

--- py_staruml/main.py
+++ py_staruml/main.py
@@ -1,18 +1,24 @@
 import argparse
+import os
+from py_staruml import StarUML
 
 def main():
     # Create the argument parser
-    parser = argparse.ArgumentParser(description='Your CLI Utility')
+    parser = argparse.ArgumentParser(description='PyStarUML')
 
-    # Add your command-line arguments here
-    # parser.add_argument('--option', help='Description of the option')
+    # Add the generateproject argument
+    parser.add_argument('--generateproject', metavar='P', type=str, help='Generate a Django project from a .mdj file')
 
     # Parse the command-line arguments
     args = parser.parse_args()
 
     # Call your functions or modules based on the command-line arguments
-    # if args.option:
-    #     do_something(args.option)
+    if args.generateproject:
+        # Create a StarUML object
+        staruml = StarUML(args.generateproject)
+
+        # Generate the Django project
+        staruml.generate_django_project()
 
 if __name__ == '__main__':
     main()
