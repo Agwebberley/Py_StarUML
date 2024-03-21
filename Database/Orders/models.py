b79e00e244393ea42664900f4ed6bd36f547fa74
Andrew Webberley <agwebberley@pm.me>
2024-03-21 06:41:38 -0700

    Update example model

--- Database/Orders/models.py
+++ Database/Orders/models.py
@@ -1,13 +1,15 @@
 from django.db import models
-from Customers.models import customers
+from Items import items
+from Customers import customers
+
+
+class orderItem(models.Model):
+    quantity = models.IntegerField()
+    item = models.ForeignKey(items, on_delete=models.CASCADE)
+    order = models.ForeignKey('orders', on_delete=models.CASCADE)
 
 
 class orders(models.Model):
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
     customer = models.ForeignKey(customers, on_delete=models.CASCADE)
-
-
-class orderItem(models.Model):
-    quantity = models.IntegerField()
-    order = models.ForeignKey('orders', on_delete=models.CASCADE)
