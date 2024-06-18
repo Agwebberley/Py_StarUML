91f9a798dd78850903deea0586b543ba1625c774
Agwebberley <agwebberley@pm.me>
2024-06-17 17:49:13 -0700

    cleanup; order_management

--- Database/Customers/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class customers(models.Model):
-    name = models.CharField(max_length=255)
-    billing_address = models.CharField(max_length=255)
-    shipping_address = models.CharField(max_length=255)
-    phone = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
